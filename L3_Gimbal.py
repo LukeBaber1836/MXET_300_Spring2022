0533551254327c3f3957953f11c1d31e34c25e98
Luke_Baber <luke.baber1@gmail.com>
2022-04-04 00:07:28 +0000

    Working on servo.py

---
+++ L3_Gimbal.py
@@ -0,0 +1,6 @@
+import time, math
+import getopt, sys
+import rcpy  # This automatically initizalizes the robotics cape
+import rcpy.servo as servo
+import rcpy.clock as clock	# For PWM period for servos
+
